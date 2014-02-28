1cf71c96e474aa49139234a26b7db8e7d37b84b9
caiwan <caiwan@5fe88468-9b50-4f28-9dd2-807ac82fe980>
2014-02-28 20:55:34 +0000

    git-svn-id: http://caiwan.dyndns.org:8080/svn/kep_es_hang/trunk@2 5fe88468-9b50-4f28-9dd2-807ac82fe980

--- FFT/FFT/main.c
+++ FFT/FFT/main.c
@@ -80,23 +80,48 @@ void DFT(const float *in, const unsigned int len, struct complex_t *out){
 // --------------------------------------------------------------
 // Fast Foutirer Transform
 // csak 2^n darab mintaval mukodik
-int FFT_radix2(const float *in, const unsigned int len, struct complex_t *out){
-	int bit_count, i, log_len;
+// http://www.katjaas.nl/FFTimplement/FFTimplement.html
+void FFT(unsigned int logN, double *real, double *im) // logN is base 2 log(N)
+{
+    unsigned int n=0, nspan, span, submatrix, node;
+    unsigned int N = 1<<logN;
+    double temp, primitive_root, angle, realtwiddle, imtwiddle;
 
-	if(!in) return 1;
-	if(!out) return 2;
-	if(!len) return 3;
+      
 
-	//quick-and-dirty megoldas annak ellenorzesere, hogy a tomb hossza 2^n hosszu
-	for(i=0; i<sizeof(len)*8; i++){
-		log_len = i; if(len >> i & 1) break;
-	}
-	if (1<<log_len != len) return 3;
-
-	// implementacio
-
-	return 0;
-}
+    for(span=N>>1; span; span>>=1)      // loop over the FFT stages
+    {
+       primitive_root = MINPI/span;     // define MINPI in the header
+       
+       for(submatrix=0; submatrix<(N>>1)/span; submatrix++)
+       {
+          for(node=0; node<span; node++)
+          {
+            nspan = n+span;
+            temp = real[n] + real[nspan];       // additions & subtractions
+            real[nspan] = real[n]-real[nspan];
+            real[n] = temp;
+            temp = im[n] + im[nspan];
+            im[nspan] = im[n] - im[nspan];
+            im[n] = temp;
+            
+            angle = primitive_root * node;      // rotations
+            realtwiddle = cos(angle);
+            imtwiddle = sin(angle);
+            temp = realtwiddle * real[nspan] - imtwiddle * im[nspan];
+            im[nspan] = realtwiddle * im[nspan] + imtwiddle * real[nspan];
+            real[nspan] = temp;
+            
+            n++;   // not forget to increment n
+            
+          } // end of loop over nodes
+         
+          n = (n+span) & (N-1);   // jump over the odd blocks
+        
+        } // end of loop over submatrices
+        
+     } // end of loop over FFT stages
+} // end of FFT function
 
 // --------------------------------------------------------------
 // main
