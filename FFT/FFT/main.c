b39c60f78b99e59403cbfd14ee98c6ef8c3bd148
caiwan <caiwan@5fe88468-9b50-4f28-9dd2-807ac82fe980>
2014-03-01 14:37:58 +0000

    git-svn-id: http://caiwan.dyndns.org:8080/svn/kep_es_hang/trunk@4 5fe88468-9b50-4f28-9dd2-807ac82fe980

--- FFT/FFT/main.c
+++ FFT/FFT/main.c
@@ -68,11 +68,11 @@ void DFT(const float *in, const unsigned int len, struct complex_t *out){
 		dk.re = dk.im = 0.0;
 		for(n=0; n<len; n++){
 			op.im = op.re = 0.0;
-			phi = -theta*(float)k*(float)n;
+			phi = -theta*(float)k*(float)n;		//printf("%3.2f; ", phi);
 			complex_exp(in[n], phi, &op);
 			complex_add(&op, &dk, &dk);
 		}
-		complex_mul_by_c(&dk, 1/(float)(len+1), &dk);
+		//complex_mul_by_c(&dk, 1/(float)(len+1), &dk);
 		out[k] = dk;
 	}
 }
@@ -81,89 +81,58 @@ void DFT(const float *in, const unsigned int len, struct complex_t *out){
 // Fast Foutirer Transform
 // csak 2^n darab mintaval mukodik
 // http://www.katjaas.nl/FFTimplement/FFTimplement.html
-void FFT(unsigned int logN, struct complex_t* in, struct complex_t* out) // logN is base 2 log(N)
+void FFT(unsigned int logN, struct complex_t* in, struct complex_t* out)
 {
-	unsigned int n=0, nspan, span, submatrix, node;
-	unsigned int N = 1<<logN;
-	double temp, primitive_root, angle, realtwiddle, imtwiddle;
-
-	struct complex_t *in_n, *in_nspan, *out_n, *out_nspan;
-
-	for(span=N>>1; span; span>>=1){
-//		primitive_root = MINPI/span;
-		primitive_root = M_PI/span;
-		for(submatrix=0; submatrix<(N>>1)/span; submatrix++){
-			for(node=0; node<span; node++){
-				nspan = n+span;
-				
-				in_n = &in[n];
-				in_nspan = &in[nspan];
-				out_nspan = &out[nspan];
-
-				temp = in_n->re + in_nspan->re;
-				out_nspan->re = in_n->re - in_nspan->re;
-
-				out_n = temp;
-
-				temp = in_n->im + in_nspan->im;
-				out_nspan->im = in_n->im - in_nspan->im;
-				out_n->im = temp;
-
-				angle = primitive_root * node;      // rotations
-				realtwiddle = cos(angle);
-				imtwiddle = sin(angle);
-
-				temp = realtwiddle * out_nspan->re - imtwiddle * out_nspan->im;
-				out_nspan->im = realtwiddle * in_nspan->im + imtwiddle * out_nspan->re;
-				out_nspan->re = temp;
-
-				n++;
-            
-          }
-         
-          n = (n+span) & (N-1);
-        
-        } 
-     } 
+	// ... 
 }
 
 void FFT2(unsigned int logN, const float* in, struct complex_t* out) // logN is base 2 log(N)
 {
-	unsigned int n=0, nspan, span, submatrix, node;
+	unsigned int i = 0, n=0, nspan, span, submatrix, node;
 	unsigned int N = 1<<logN;
 	double temp, primitive_root, angle, realtwiddle, imtwiddle;
 
-	struct complex_t in_n, in_nspan, *out_n, *out_nspan;
+	struct complex_t *out_n, *out_nspan;
+
+	for(i=0; i<N; i++) {out[i].re = in[i]; out[i].im = 0;}
 
 	for(span=N>>1; span; span>>=1){
 
-		primitive_root = M_PI/span;
+		primitive_root = 2*M_PI/span;
 		for(submatrix=0; submatrix<(N>>1)/span; submatrix++){
 			for(node=0; node<span; node++){
 				nspan = n+span;
 				
-				in_n.re = &in[n];		in_n.im = 0.0f;
-				in_nspan.re = &in[nspan];	in_nspan.im = 0.0f;
+				//in_n.re = in[n];		in_n.im = 0.0f;
+				//in_nspan.re = in[nspan];	in_nspan.im = 0.0f;
 
 				out_nspan = &out[nspan];
 				out_n = &out[n];
 
-				temp = in_n.re + in_nspan.re;
-				out_nspan->re = in_n.re - in_nspan.re;
+//				temp = real[n] + real[nspan];       // additions & subtractions
+				temp = out_n->re + out_nspan->re;
+//				real[nspan] = real[n]-real[nspan];
+				out_nspan->re = out_n->re - out_nspan->re;
+///				real[n] = temp;
+				out_n->re = temp;
 
-				out_n = temp;
-
-				temp = in_n.im + in_nspan.im;
-				out_nspan->im = in_n.im - in_nspan.im;
+//				temp = im[n] + im[nspan];
+				temp = out_n->im + out_nspan->im;
+//				im[nspan] = im[n] - im[nspan];
+				out_nspan->im = out_n->im - out_nspan->im;
+//				im[n] = temp;
 				out_n->im = temp;
 
 				angle = primitive_root * node;      // rotations
 				realtwiddle = cos(angle);
-				imtwiddle = sin(angle);
+				imtwiddle = sin(angle); //printf("%3.2f ", angle);
 
+//				temp = realtwiddle * real[nspan] - imtwiddle * im[nspan];
 				temp = realtwiddle * out_nspan->re - imtwiddle * out_nspan->im;
-				out_nspan->im = realtwiddle * in_nspan->im + imtwiddle * out_nspan->re;
-				out_nspan->re = temp;
+//				im[nspan] = realtwiddle * im[nspan] + imtwiddle * real[nspan];
+				out_nspan->im = realtwiddle * out_nspan->im + imtwiddle * out_nspan->re;
+//				real[nspan] = temp;
+				out_nspan->re = temp;				
 
 				n++;
             
@@ -178,21 +147,23 @@ void FFT2(unsigned int logN, const float* in, struct complex_t* out) // logN is 
 // --------------------------------------------------------------
 // main
 
-float data[] = {0.f,1.f,2.f,3.f};
+float data[] = {0.f,1.f,2.f,3.f,4.f,5.f,6.f,7.f};
 
 int main(){
 	int datasize = sizeof(data)/sizeof(*data),
 		i = 0;
-	struct complex_t *res_dft = (struct complex_*)malloc(datasize*sizeof(*res_dft));
-	struct complex_t *res_fft = (struct complex_*)malloc(datasize*sizeof(*res_dft));
+	struct complex_t *res_dft = (struct complex_t*)malloc(datasize*sizeof(*res_dft));
+	struct complex_t *res_fft = (struct complex_t*)malloc(datasize*sizeof(*res_fft));
 
 	DFT(data, datasize, res_dft);
-	FFT2(2, data, res_fft);
+	FFT2(, data, res_fft);
 
+	printf("DFT:\n");
 	for(i=0; i<datasize; i++){
 		printf("D[%d] = %3.2f %c j*%3.2f \n", i, res_dft[i].re, (res_dft[i].im<0)?'-':'+', (res_dft[i].im<0)?-res_dft[i].im:res_dft[i].im);
 	}
 
+	printf("\nRadix-2 FFT:\n");
 	for(i=0; i<datasize; i++){
 		printf("D[%d] = %3.2f %c j*%3.2f \n", i, res_fft[i].re, (res_fft[i].im<0)?'-':'+', (res_fft[i].im<0)?-res_fft[i].im:res_fft[i].im);
 	}
